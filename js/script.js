e8d9d5ca6d3e775e79811eb02e2bc7f0945a2cba
Richard Rugless <r.rugless@gmail.com>
2020-10-12 10:21:15 -0700

    added handling for "shoot" click

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 // *************************** YOUR CODE BELOW *******************************
+document.getElementById("shoot").onclick = function() {
+  document.getElementById("userChoice").innerText = document.getElementById("input").value;
+}
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
