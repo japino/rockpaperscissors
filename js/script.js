6d293936f1b44175c1be752d7c7281c15ad1b17b
Richard Rugless <r.rugless@gmail.com>
2020-10-12 10:24:04 -0700

    added logging

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // *************************** YOUR CODE BELOW *******************************
 document.getElementById("shoot").onclick = function() {
-  document.getElementById("userChoice").innerText = document.getElementById("input").value;
+  var choice = document.getElementById("input").value;
+  console.log(choice)
+  document.getElementById("userChoice").innerText = choice;
 }
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
